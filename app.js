cb67faa64d65b08abbb2926a41efc1cd5e988347
KritikaJain1065 <160481844+KritikaJain1065@users.noreply.github.com>
2025-04-03 14:38:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const url="https://medi-assist.onrender.com"
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
